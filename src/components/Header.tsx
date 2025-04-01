7e37e24167f1935b613f37ef0a18ba406e4a9d8c
mdsajalali <mdsajalali2020@gmail.com>
2025-04-01 22:00:06 +0600

    update: header ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
 import { HeaderProps } from "@/types/main.types";
 import { Menu, X, Users, Bell } from "lucide-react";
 
-
-
 const Header = ({ setIsSidebarOpen, isSidebarOpen }: HeaderProps) => {
   return (
-    <header className="bg-white shadow-sm z-[999] flex items-center justify-between px-4 py-3 h-16 fixed top-0 left-0 right-0">
+    <header className="bg-white border-b border-gray-300 z-[999] flex items-center justify-between px-4 md:px-8 lg:px-10 py-3 h-16 fixed top-0 left-0 right-0">
       <div className="flex items-center space-x-3">
         <button
           className="md:hidden"
@@ -17,17 +15,19 @@ const Header = ({ setIsSidebarOpen, isSidebarOpen }: HeaderProps) => {
           Teacher‘s Center
         </h1>
       </div>
-      <div className="flex items-center space-x-3">
-        <button className="p-2 rounded-full hover:bg-gray-100">
+      <div className="flex items-center md:space-x-3">
+        <button className="p-2 rounded-full cursor-pointer hover:bg-gray-100">
           <Users size={20} className="text-gray-600" />
         </button>
-        <button className="p-2 rounded-full hover:bg-gray-100">
+        <button className="p-2 rounded-full cursor-pointer hover:bg-gray-100">
           <Bell size={20} className="text-gray-600" />
         </button>
-        <div className="flex items-center space-x-2">
-          <div className="w-8 h-8 bg-gray-300 rounded-full"></div>
+        <div className="border-l h-10 pl-2"></div>
+        <div className="flex items-center md:space-x-3">
+          <div className="w-8 h-8 bg-gray-300 cursor-pointer rounded-full"></div>
           <span className="text-sm font-medium text-gray-800 hidden md:block">
-            Sir 1 | Chemistry | Level 3
+            Sir 1 <br />{" "}
+            <span className="font-normal text-black">Chemistry | Level 3</span>
           </span>
         </div>
       </div>
