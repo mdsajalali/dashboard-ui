3410e80443c28a6ff9b30695de7dac59c19999af
mdsajalali <mdsajalali2020@gmail.com>
2025-04-02 00:09:15 +0600

    added: images

--- src/components/CourseSelection.tsx
+++ src/components/CourseSelection.tsx
@@ -1,4 +1,5 @@
 import { Book, ChevronDown } from "lucide-react";
+import Image from "next/image";
 
 const CourseSelection = () => {
   return (
@@ -33,8 +34,20 @@ const CourseSelection = () => {
           Take Class
         </button>
         <button className="flex  items-center space-x-2   rounded-md px-3 py-2  text-sm text-gray-600 w-full md:w-auto">
-          <div className="size-[35px] z-[9] bg-gray-300 rounded-full"></div>
-          <div className="size-[35px] -ml-[18px] z-[9] bg-black rounded-full"></div>
+          <Image
+            src="https://thumbs.dreamstime.com/b/d-icon-avatar-cute-smiling-woman-cartoon-hipster-character-people-close-up-portrait-isolated-transparent-png-background-352288997.jpg"
+            alt="User"
+            width={35}
+            height={35}
+            className="size-[35px] z-[9] bg-gray-300   rounded-full cursor-pointer"
+          />
+          <Image
+            src="https://img.freepik.com/premium-vector/boy-with-blue-hoodie-blue-hoodie-with-hoodie-it_1230457-42660.jpg?semt=ais_hybrid"
+            alt="User"
+            width={35}
+            height={35}
+            className="size-[35px]   cursor-pointer  -ml-[22px] z-[9] bg-black rounded-full"
+          />
           <span className="bg-white -ml-[54px] pl-14 pr-3  py-2 rounded-md">
             Add TA
           </span>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { HeaderProps } from "@/types/main.types";
 import { Menu, X, Users, Bell } from "lucide-react";
+import Image from "next/image";
 
 const Header = ({ setIsSidebarOpen, isSidebarOpen }: HeaderProps) => {
   return (
@@ -24,7 +25,13 @@ const Header = ({ setIsSidebarOpen, isSidebarOpen }: HeaderProps) => {
         </button>
         <div className="border-l h-10 pl-2"></div>
         <div className="flex items-center md:space-x-3">
-          <div className="w-8 h-8 bg-gray-300 cursor-pointer rounded-full"></div>
+          <Image
+            src="https://img.freepik.com/free-vector/smiling-young-man-illustration_1308-174669.jpg"
+            alt="User"
+            width={40}
+            height={40}
+            className="size-10 rounded-full cursor-pointer"
+          />
           <span className="text-sm font-medium text-gray-800 hidden md:block">
             Sir 1 <br />{" "}
             <span className="font-normal text-black">Chemistry | Level 3</span>
